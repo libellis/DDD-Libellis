3408964f2581824494b6e10b0da50a223aeb5b8e
Peter Farr <Peter@PrismaPhonic.com>
2019-05-01 09:54:47 -0700

    Added many new invariance checks to thoroughly check that Election aggregate root enforces invariance and maintains election business logic

--- src/ElectionSystem/Tests/Core/Domain/Model/Aggregates/Election.spec.js
+++ src/ElectionSystem/Tests/Core/Domain/Model/Aggregates/Election.spec.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var chai_1 = require("chai");
 require("mocha");
@@ -16,6 +27,64 @@ describe('test invariance enforcement by root', function () {
         var fakeVoter = TestVoterFactory_model_1.TestVoterFactory.createTestVoter();
         var ballotData = TestBallotDataFactory_model_1.TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
         election.castBallot(faker.random.uuid, ballotData);
+        // simulating the same voter creating a different ballot, but with the same voterId
+        var secondBallotData = TestBallotDataFactory_model_1.TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+        chai_1.expect(function () { election.castBallot(faker.random.uuid, secondBallotData); }).to.throw();
+    });
+    it('should not allow a voter to cast a ballot with duplicate questions', function () {
+        var masterBallot = TestMasterBallotFactory_model_1.TestMasterBallotFactory.createFullMasterBallot();
+        var start = new Date();
+        var end = new Date((new Date()).getTime() + 30);
+        var election = TestElectionFactory_model_1.TestElectionFactory.createElectionWithFactoryMethod(masterBallot, { start: start, end: end });
+        var fakeVoter = TestVoterFactory_model_1.TestVoterFactory.createTestVoter();
+        var ballotData = TestBallotDataFactory_model_1.TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+        var questionCopy = __assign({}, ballotData.voteData.questionsData[0]);
+        ballotData.voteData.questionsData.push(questionCopy);
+        chai_1.expect(function () { election.castBallot(faker.random.uuid, ballotData); }).to.throw();
+    });
+    it('should not allow a voter to cast a ballot with duplicate choices', function () {
+        var masterBallot = TestMasterBallotFactory_model_1.TestMasterBallotFactory.createFullMasterBallot();
+        var start = new Date();
+        var end = new Date((new Date()).getTime() + 30);
+        var election = TestElectionFactory_model_1.TestElectionFactory.createElectionWithFactoryMethod(masterBallot, { start: start, end: end });
+        var fakeVoter = TestVoterFactory_model_1.TestVoterFactory.createTestVoter();
+        var ballotData = TestBallotDataFactory_model_1.TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+        var choiceCopy = __assign({}, ballotData.voteData.questionsData[0].choicesData[0]);
+        // mutate question to hold duplicates of the choice
+        ballotData.voteData.questionsData[0].choicesData.push(choiceCopy);
+        chai_1.expect(function () { election.castBallot(faker.random.uuid, ballotData); }).to.throw();
+    });
+    it('should not allow a voter to cast a ballot with missing questions', function () {
+        var masterBallot = TestMasterBallotFactory_model_1.TestMasterBallotFactory.createFullMasterBallot();
+        var start = new Date();
+        var end = new Date((new Date()).getTime() + 30);
+        var election = TestElectionFactory_model_1.TestElectionFactory.createElectionWithFactoryMethod(masterBallot, { start: start, end: end });
+        var fakeVoter = TestVoterFactory_model_1.TestVoterFactory.createTestVoter();
+        var ballotData = TestBallotDataFactory_model_1.TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+        ballotData.voteData.questionsData.pop();
+        chai_1.expect(function () { election.castBallot(faker.random.uuid, ballotData); }).to.throw();
+    });
+    it('should not allow a voter to cast a ballot with missing choices', function () {
+        var masterBallot = TestMasterBallotFactory_model_1.TestMasterBallotFactory.createFullMasterBallot();
+        var start = new Date();
+        var end = new Date((new Date()).getTime() + 30);
+        var election = TestElectionFactory_model_1.TestElectionFactory.createElectionWithFactoryMethod(masterBallot, { start: start, end: end });
+        var fakeVoter = TestVoterFactory_model_1.TestVoterFactory.createTestVoter();
+        var ballotData = TestBallotDataFactory_model_1.TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+        ballotData.voteData.questionsData[0].choicesData.pop();
+        chai_1.expect(function () { election.castBallot(faker.random.uuid, ballotData); }).to.throw();
+    });
+    it('should not allow a voter to cast a ballot with choices from unrelated master ballots', function () {
+        var masterBallot = TestMasterBallotFactory_model_1.TestMasterBallotFactory.createFullMasterBallot();
+        var start = new Date();
+        var end = new Date((new Date()).getTime() + 30);
+        var election = TestElectionFactory_model_1.TestElectionFactory.createElectionWithFactoryMethod(masterBallot, { start: start, end: end });
+        var fakeVoter = TestVoterFactory_model_1.TestVoterFactory.createTestVoter();
+        var ballotData = TestBallotDataFactory_model_1.TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+        var differentMasterBallot = TestMasterBallotFactory_model_1.TestMasterBallotFactory.createFullMasterBallot();
+        var differentBallotData = TestBallotDataFactory_model_1.TestBallotDataFactory.createTestBallot(fakeVoter.id, differentMasterBallot);
+        var foreignChoice = __assign({}, differentBallotData.voteData.questionsData[0].choicesData[0]);
+        ballotData.voteData.questionsData[0].choicesData.push(foreignChoice);
         chai_1.expect(function () { election.castBallot(faker.random.uuid, ballotData); }).to.throw();
     });
 });
--- src/ElectionSystem/Tests/Core/Domain/Model/Aggregates/Election.spec.ts
+++ src/ElectionSystem/Tests/Core/Domain/Model/Aggregates/Election.spec.ts
@@ -19,6 +19,92 @@ describe('test invariance enforcement by root', () => {
 		const ballotData = TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
 
 		election.castBallot(faker.random.uuid, ballotData);
+
+		// simulating the same voter creating a different ballot, but with the same voterId
+		const secondBallotData = TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+
+		expect(() => { election.castBallot(faker.random.uuid, secondBallotData) }).to.throw();
+	});
+
+	it('should not allow a voter to cast a ballot with duplicate questions', () => {
+		const masterBallot = TestMasterBallotFactory.createFullMasterBallot();
+
+		const start = new Date();
+		const end = new Date((new Date()).getTime() + 30);
+		const election = TestElectionFactory.createElectionWithFactoryMethod(masterBallot, { start, end });
+
+		const fakeVoter = TestVoterFactory.createTestVoter();
+		const ballotData = TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+		const questionCopy = { ...ballotData.voteData.questionsData[0] };
+
+		ballotData.voteData.questionsData.push(questionCopy);
+
+		expect(() => { election.castBallot(faker.random.uuid, ballotData) }).to.throw();
+	});
+
+	it('should not allow a voter to cast a ballot with duplicate choices', () => {
+		const masterBallot = TestMasterBallotFactory.createFullMasterBallot();
+
+		const start = new Date();
+		const end = new Date((new Date()).getTime() + 30);
+		const election = TestElectionFactory.createElectionWithFactoryMethod(masterBallot, { start, end });
+
+		const fakeVoter = TestVoterFactory.createTestVoter();
+		const ballotData = TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+		const choiceCopy = { ...ballotData.voteData.questionsData[0].choicesData[0] };
+
+		// mutate question to hold duplicates of the choice
+		ballotData.voteData.questionsData[0].choicesData.push(choiceCopy);
+
+		expect(() => { election.castBallot(faker.random.uuid, ballotData) }).to.throw();
+	});
+
+	it('should not allow a voter to cast a ballot with missing questions', () => {
+		const masterBallot = TestMasterBallotFactory.createFullMasterBallot();
+
+		const start = new Date();
+		const end = new Date((new Date()).getTime() + 30);
+		const election = TestElectionFactory.createElectionWithFactoryMethod(masterBallot, { start, end });
+
+		const fakeVoter = TestVoterFactory.createTestVoter();
+		const ballotData = TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+
+		ballotData.voteData.questionsData.pop();
+
+		expect(() => { election.castBallot(faker.random.uuid, ballotData) }).to.throw();
+	});
+
+	it('should not allow a voter to cast a ballot with missing choices', () => {
+		const masterBallot = TestMasterBallotFactory.createFullMasterBallot();
+
+		const start = new Date();
+		const end = new Date((new Date()).getTime() + 30);
+		const election = TestElectionFactory.createElectionWithFactoryMethod(masterBallot, { start, end });
+
+		const fakeVoter = TestVoterFactory.createTestVoter();
+		const ballotData = TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+
+		ballotData.voteData.questionsData[0].choicesData.pop();
+
+		expect(() => { election.castBallot(faker.random.uuid, ballotData) }).to.throw();
+	});
+
+	it('should not allow a voter to cast a ballot with choices from unrelated master ballots', () => {
+		const masterBallot = TestMasterBallotFactory.createFullMasterBallot();
+
+		const start = new Date();
+		const end = new Date((new Date()).getTime() + 30);
+		const election = TestElectionFactory.createElectionWithFactoryMethod(masterBallot, { start, end });
+
+		const fakeVoter = TestVoterFactory.createTestVoter();
+		const ballotData = TestBallotDataFactory.createTestBallot(fakeVoter.id, masterBallot);
+
+		const differentMasterBallot = TestMasterBallotFactory.createFullMasterBallot();
+		const differentBallotData = TestBallotDataFactory.createTestBallot(fakeVoter.id, differentMasterBallot);
+		const foreignChoice = { ...differentBallotData.voteData.questionsData[0].choicesData[0] };
+
+		ballotData.voteData.questionsData[0].choicesData.push(foreignChoice);
+
 		expect(() => { election.castBallot(faker.random.uuid, ballotData) }).to.throw();
 	});
 });
